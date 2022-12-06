cb24c8923c9316ba1abb1ff908a248226cb829b0
Lukasz Stasko <lukstasko@gmail.com>
2022-12-06 22:18:12 +0100

    6.1 Added kodilla-testing module

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,9 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+
 }
 
 tasks.withType<Test> {
